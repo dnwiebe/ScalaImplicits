c79bce85d37686161cd247216cfb05288e7c9d3e
Dan Wiebe <dnwiebe@gmail.com>
2015-06-21 22:43:58 -0400

    Slides, initial revision

--- src/test/scala/parameters/implicitp/MultipleParameterListsTest.scala
+++ src/test/scala/parameters/implicitp/MultipleParameterListsTest.scala
@@ -9,12 +9,12 @@ class MultipleParameterListsTest extends path.FunSpec {
   class BalanceTaxation (percent: Int) extends Taxation (percent)
   class IncomeTaxation (percent: Int) extends Taxation (percent)
 
-  def add (balance: Int)(wage: Int)(implicit bt: BalanceTaxation, ic: IncomeTaxation) = bt.tax (balance) + ic.tax (wage)
+  def earn (balance: Int)(wage: Int)(implicit bt: BalanceTaxation, ic: IncomeTaxation) = bt.tax (balance) + ic.tax (wage)
 
   it ("Two implicit parameters in one list") {
     implicit val bt = new BalanceTaxation (70)
     implicit val it = new IncomeTaxation (120)
-    val result = add (40)(50)(bt, it)
+    val result = earn (40)(50)(bt, it)
 
     assert (result === 28 + 60)
   }
